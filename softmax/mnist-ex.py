5fe37023559349c9b26e547524fb7cf6d33450af
bkuk69 <richku@gmail.com>
2018-08-08 09:24:36 +0900

    Mnist

---
+++ softmax/mnist-ex.py
@@ -0,0 +1,8 @@
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("data/MNIST", one_hot="True")
+images = mnist.test.images
+labels = mnist.test.labels
+
+images = images.reshape([-1,28,28])
+print(images.shape)
+print(images[0])
